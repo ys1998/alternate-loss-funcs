5b6111974427d2ecea6ff6c4dca9e3616bb8595a
Yash <yashshah2398@gmail.com>
2018-02-02 22:57:10 +0530

    Updated Saver

--- eval.py
+++ eval.py
@@ -61,8 +61,8 @@ def evaluate(args):
     print(LOGS[4])
 
     with tf.Session() as sess:
-        tf.initialize_all_variables().run()
-        saver = tf.train.Saver(tf.all_variables())
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
         # restore model
         saver.restore(sess, ckpt.model_checkpoint_path)
         run_eval_epoch(sess=sess,
