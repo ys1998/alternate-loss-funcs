de298a47315cbc285d2a872bc68bd431defc9fef
Yash <yashshah2398@gmail.com>
2018-03-30 01:10:50 +0530

    Added SBATCH script

--- slurm.sh
+++ slurm.sh
@@ -5,3 +5,7 @@
 #SBATCH --output=slurm-%j.out
 
 ## Write your command here
+
+cd ~/exp;
+
+python main.py --mode train --data_dir wiki/ --dataset wiki --save_dir save/ --best_dir save_best --config_file config/sgd.yml --lm ngram-lm --loss_mode mixed --mixed_constant 0.4 --job_id wiki_int_l2_0;
