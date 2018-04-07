cdc7a1ab36a0504d48dc4d41b2bee37d314de9ca
Yash <yashshah2398@gmail.com>
2018-04-07 23:39:44 +0530

    Added slurm output files and modified script

--- slurm.sh
+++ slurm.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --time=360
-#SBATCH --gres=gpu:4,gpu_mem:1500M
-#SBATCH --cpus-per-task=48
+#SBATCH --gres=gpu:4,gpu_mem:2500M
 #SBATCH --output=logs/slurm-%j.out
 
 ## Write your command here
 
-python main.py --mode train --data_dir wiki/ --dataset wiki --save_dir save/ --best_dir save_best --config_file config/sgd.yml --lm ngram-lm --loss_mode mixed --mixed_constant 0.4 --job_id wiki_int_l2_0
+python main.py --mode train --data_dir wiki/ --dataset wiki --save_dir save/ --best_dir save_best --config_file config/sgd.yml --lm ngram-lm --loss_mode mixed --mixed_constant 0.4 --job_id wiki_int_l2_0 --device gpu
